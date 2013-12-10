561ee3da3690044a2feb97437bd2c214a36b3338
Michael Gernoth <michael@gernoth.net>
2013-12-10 13:32:12 +0100

    Make it possible to only build hostapd by checking for BOARD_HOSTAPD_DRIVER_TI
    Change-Id: I37c48be4b181cb24e769d703792733eefa944a8e

--- Android.mk
+++ Android.mk
@@ -1,3 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+
 ifeq ($(WPA_SUPPLICANT_VERSION),VER_0_8_X_TI)
     include $(call all-subdir-makefiles)
+else
+    ifneq ($(BOARD_HOSTAPD_DRIVER_TI),)
+        include $(LOCAL_PATH)/hostapd/Android.mk
+    endif
 endif
